24b5f83a5fdd3094e4145b5be6ce7fcb629957d7
shunichiHato <81155721+shunichiHato@users.noreply.github.com>
2025-11-10 10:51:50 +0900

    Insert pdb breakpoint in reporting.py
    Added pdb breakpoint for debugging notebook execution.

--- aizynthtrain/utils/reporting.py
+++ aizynthtrain/utils/reporting.py
@@ -8,8 +8,8 @@ import nbformat
 import jupytext
 import papermill
 from ploomber_engine import execute_notebook
-
 from papermill.engines import NotebookExecutionManager
+import pdb
 
 _orig_notebook_complete = NotebookExecutionManager.notebook_complete
 
@@ -50,6 +50,7 @@ def create_html_report_from_notebook(
     notebook = jupytext.read(notebook_path, fmt="py:percent")
     jupytext.write(notebook, input_notebook, fmt="ipynb")
 
+    pdb.set_trace()
     execute_notebook(input_notebook, output_notebook, parameters=parameters)
 
     with open(output_notebook, "r") as fileobj:
